ce4d20a21c0b542dd6cb11d09d2447bdce299ed3
Bharathi25raj <bharathi25raj@gmail.com>
2025-04-18 07:34:44 +0530

    Database Connection

--- src/main/java/com/bharathi/productservice/models/Category.java
+++ src/main/java/com/bharathi/productservice/models/Category.java
@@ -1,13 +1,18 @@
 package com.bharathi.productservice.models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 
 @Data
+@Entity
 public class Category {
 
+    @Id
     private Long id;
     private String title;
+
 
 }
--- src/main/java/com/bharathi/productservice/models/Product.java
+++ src/main/java/com/bharathi/productservice/models/Product.java
@@ -1,20 +1,40 @@
 package com.bharathi.productservice.models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 
 @Setter
 @Getter
+@Entity
 public class Product {
 
+    @Id
     private Long id;
     private String title;
     private String description;
+
+    @ManyToOne
     private Category category;
     private Double price;
     private String image;
 
+   /*
+       1                    1
+
+    Product ------------ Category
+
+       M                     1
+
+       ==> M:1
+
+    */
 }
